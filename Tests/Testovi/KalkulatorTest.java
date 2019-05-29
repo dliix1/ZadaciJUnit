97c6f33ff8e57272a515ba78f6bfd388f5b550d4
Zuhdija Delic <zuka_delic@hotmail.com>
2019-05-29 12:29:02 +0200

    Added some stuff.

--- Tests/Testovi/KalkulatorTest.java
+++ Tests/Testovi/KalkulatorTest.java
@@ -6,29 +6,29 @@ import org.junit.Test;
 import Zadaci.Kalkulator;
 
 public class KalkulatorTest {
-	
+
 	@Test
-	public void testSabiranje(){
+	public void testSabiranje() {
 		int rezultat = Kalkulator.sabiranje(2, 3);
-		assertEquals(5, rezultat);
+		assertEquals(rezultat, 5);
 	}
-	
+
 	@Test
-	public void testOduzimanje(){
+	public void testOduzimanje() {
 		int rezultat = Kalkulator.oduzimanje(5, 2);
-		assertEquals(3, rezultat);
+		assertEquals(rezultat, 3);
 	}
-	
+
 	@Test
-	public void testMnozenje(){
+	public void testMnozenje() {
 		int rezultat = Kalkulator.mnozenje(3, 2);
-		assertEquals(6, rezultat);
+		assertEquals(rezultat, 6);
 	}
-	
+
 	@Test
-	public void testDijeljenje(){
+	public void testDijeljenje() {
 		int rezultat = Kalkulator.dijeljenje(10, 2);
-		assertEquals(5, rezultat);
+		assertEquals(rezultat, 5);
 	}
 
 }
--- Tests/Testovi/KonverterTest.java
+++ Tests/Testovi/KonverterTest.java
@@ -7,37 +7,37 @@ import Zadaci.Konverter;
 public class KonverterTest {
 
 	@Test
-	public void testKilogramUFunte(){
+	public void testKilogramUFunte() {
 		double rezultat = Konverter.izKilogramaUFunte(1.0);
 		assertEquals(rezultat, 2.046, 0.1);
 	}
-	
+
 	@Test
-	public void testFuntaUKilogram(){
+	public void testFuntaUKilogram() {
 		double rezultat = Konverter.izFunteUKilogram(2.046);
 		assertEquals(rezultat, 1.0, 0.1);
 	}
-	
+
 	@Test
-	public void testCentimetriUInce(){
+	public void testCentimetriUInce() {
 		double rezultat = Konverter.izCentimetaraUInce(1.0);
 		assertEquals(rezultat, 0.39, 0.1);
 	}
-	
+
 	@Test
-	public void testInciUCentimetre(){
+	public void testInciUCentimetre() {
 		double rezultat = Konverter.izIncaUCentimetre(0.39);
 		assertEquals(rezultat, 1.0, 0.1);
 	}
-	
+
 	@Test
-	public void testCelzijusUFarenhajt(){
+	public void testCelzijusUFarenhajt() {
 		double rezultat = Konverter.izCelzijusaUFarenhajte(1.0);
 		assertEquals(rezultat, 33.8, 0.1);
 	}
-	
+
 	@Test
-	public void testFarenhajtUCelzijus(){
+	public void testFarenhajtUCelzijus() {
 		double rezultat = Konverter.izFarenhajtiUCelzijuse(33.8);
 		assertEquals(rezultat, 1.0, 0.1);
 	}
